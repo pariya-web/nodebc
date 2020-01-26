70471c95b44561adf5d1cdd66b71100f78c41a9f
unknown <p_eghdami@yahoo.com>
2020-01-26 11:37:41 -0800

    added index 2

---
+++ js/ex01/index2.js
@@ -0,0 +1,24 @@
+/*Declare the following values:
+
+First name
+Last name
+Age
+Date of birth
+Address
+Assign a value to each variable using the right variable name
+
+Your first name (string)
+Your last name (string)
+Your age (number)
+Your date of birth (string, format mm/dd/yyyy)
+Your address (string)
+TIP: Use single or double quotes around the text for strings
+*/
+let firstName = 'Pariya';
+let lastName = 'Eghdami';
+let age = 33;
+let datOfBirth = '19 March 1986';
+let address = '847 Hornby Street';
+
+console.log(firstName);
+
