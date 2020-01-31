557bfcd3ce55f9df2939206fd36b65ff761bd95c
unknown <p_eghdami@yahoo.com>
2020-01-30 18:29:01 -0800

    index 3 added

---
+++ js/ex01/index3.js
@@ -0,0 +1,24 @@
+/* Create a new index3.js file
+
+Declare the following values using only one line:
+
+First name
+Last name
+Age
+Date of birth
+Address
+Assign a value to each variable using the right variable name
+
+Your first name (string)
+Your last name (string)
+Your age (number)
+Your date of birth (string, format mm/dd/yyyy)
+Your address (string)
+TIP: Use single or double quotes for strings
+*/
+var firstName, lastName, age, dateOfBirth, address;
+firstName = 'Pariya';
+lastName = 'Eghdami'
+age = 33;
+dateOfBirth = 'March/19/1986';
+address = '847 Hornby street';
