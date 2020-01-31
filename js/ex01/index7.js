896f9e8aba887402beeff494b54eacfbb34f88bc
unknown <p_eghdami@yahoo.com>
2020-01-30 18:30:42 -0800

    index 7 added

---
+++ js/ex01/index7.js
@@ -0,0 +1,9 @@
+/* Create a new index7.js file
+Copy and paste the code from ex_6
+Refactor your code to use let instead of var*/
+let firstname = 'Pariya';
+let lastname = 'Eghdami';
+let age = 33;
+let dateOfBirth = 'March/19/1986';
+let address = '847 Hornby Street';
+console.log(firstname);
