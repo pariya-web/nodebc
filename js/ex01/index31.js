bea67bdc5b2335a6826b98f543da75e691b90cbd
unknown <p_eghdami@yahoo.com>
2020-01-30 18:38:57 -0800

    index 31 added

---
+++ js/ex01/index31.js
@@ -0,0 +1,13 @@
+/* Create a new index31.js file
+Calculate and show the perimeter of a triangle
+First side is 10
+Second side is 20
+Third side is 5
+Show a descriptive message to show the triangle perimeter and sides length*/
+let firstside = 10;
+let secondside = 20;
+let thirdside = 5;
+let perimeterofatrinagle = firstside + secondside + thirdside;
+let templateoftringle = `When the firstside of a triangle is ${firstside} and the second side is ${secondside} and
+the third side is ${thirdside}, you add the three side together and the primeter would be ${perimeterofatrinagle}.`;
+console.log(templateoftringle);
