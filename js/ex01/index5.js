708724d33d3810bef87742b486aa339baaf9e760
unknown <p_eghdami@yahoo.com>
2020-01-30 18:30:15 -0800

    index 5 added

---
+++ js/ex01/index5.js
@@ -0,0 +1,14 @@
+/*Create a new index5.js file
+Copy and paste the code from ex_4
+Show each variable value using console.log()*/
+var firstname = 'Pariya';
+var lastname = 'Eghdami';
+var age = 33;
+var dateOfBirth = 'March/19/1986';
+var address = '847 Hornby Street';
+
+console.log(firstname);
+console.log(lastname);
+console.log(age);
+console.log(dateOfBirth);
+console.log(address);
