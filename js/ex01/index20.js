7bc4ddcd805d32b3896e0fa8f4b87d2ee9401b4d
unknown <p_eghdami@yahoo.com>
2020-01-30 18:31:18 -0800

    index 20 added

---
+++ js/ex01/index20.js
@@ -0,0 +1,12 @@
+/*Create a new index20.js file
+Define a day variable and assign todays day number (number)
+Define a month variable and assign todays month number (number) (from 1 to 12)
+Define a year variable and assign todays year (number) (4 digits)
+Use a template variable to show the date with the following format: d/m/yyyy
+*/
+var day = 27;
+var month = 1;
+var year = 2020;
+var template = `${day}/${month}/${year}`;
+ console.log(template);
+ 
