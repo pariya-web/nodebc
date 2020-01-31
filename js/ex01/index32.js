3da584a67a737d3534ab27a425f9c5f0c77f10c7
unknown <p_eghdami@yahoo.com>
2020-01-30 18:39:17 -0800

    index 32 added

---
+++ js/ex01/index32.js
@@ -0,0 +1,12 @@
+/*Create a new index32.js file
+Define a height variable and assig the following value: 10
+Define a base variable and assig the following value: 4
+Calculate the perimiter of the rectangular
+Calculate the area of the rectangular
+Show both calculation results, height and base length
+Use a descriptive message to show all the values*/
+let height = 10;
+let base = 4;
+let perimeterofarectangular = height + base;
+let areaofarectangular = height * base;
+console.log('Height=', height, 'Base=', base, 'Perimeter Of the rectangular=' ,perimeterofarectangular, 'Area of the rectangular=' , areaofarectangular);
