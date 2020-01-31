7c8e8cc2295197799edf111c0846813e973955fe
unknown <p_eghdami@yahoo.com>
2020-01-30 18:38:12 -0800

    index 29 added

---
+++ js/ex01/index29.js
@@ -0,0 +1,10 @@
+/* Create a new index29.js file
+Calculate and show the perimeter of a square
+Side length is 10
+Show a descriptive message to show the square perimeter and side length*/
+let sideLenght = 10;
+const squarePrimeter = 4;
+let result = sideLenght * squarePrimeter
+let templateforperimeterofasquare = `If the side lenght of a square is ${sideLenght}, then you multiply it by ${squarePrimeter}
+and the result would be ${result}.`;
+console.log(templateforperimeterofasquare);
