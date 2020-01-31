35e195edb887d558c28a02d98ca76c8df9f72957
unknown <p_eghdami@yahoo.com>
2020-01-30 18:38:35 -0800

    index 30 added

---
+++ js/ex01/index30.js
@@ -0,0 +1,9 @@
+/*Create a new index30.js file
+Calculate and show the area of a square
+Side length is 5
+Show a descriptive message to show the square area and side length*/
+let sideLenght = 5;
+let squareArea = sideLenght * sideLenght;
+let templatesquare = `If the side lenght of a square is ${sideLenght}, you multiply it by itself and area of that square would be ${squareArea}.`;
+console.log(templatesquare);
+
