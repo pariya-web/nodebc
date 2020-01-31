484704903ab0ebad932b4de33f3fe46ce73e65b7
unknown <p_eghdami@yahoo.com>
2020-01-30 18:29:44 -0800

    index 4 added

---
+++ js/ex01/index4.js
@@ -0,0 +1,12 @@
+/*Create a new index4.js file
+Declare the following values and assign a value (one per line)
+First name / Your first name (string)
+Last name / Your last name (string)
+Age / Your age (number)
+Date of birth / Your date of birth (string, format mm/dd/yyyy)
+Address / Your address (string)*/
+var firstname = 'Pariya';
+var lastname = 'Eghdami';
+var age = 33;
+var dateOfBirth = 'March/19/1986';
+var address = '847 Hornby Street';
