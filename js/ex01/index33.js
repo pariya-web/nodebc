0783545e3e5f786ee325e505da087f4ca42a4836
unknown <p_eghdami@yahoo.com>
2020-01-30 18:39:41 -0800

    index 33 added

---
+++ js/ex01/index33.js
@@ -0,0 +1,23 @@
+/* Create a new index33.js file
+Define the userCount variable and assign the following value: 100
+Increment the user count by 5
+Show the following output: Users count: %userCount%
+Decrement the user count by 3
+Show the following output: Users count: %userCount%
+We bought a new company so now we double the user count
+Show the following output: Users count: %userCount%
+We know that 50% of the users identify as men and the other 50% identify as women
+Show the following output: Users count: %userCount%, %menCount% % & %womenCount% %
+Use as many assignment operators as you can*/
+let userCount = 100;
+userCount += 5;
+console.log('Users count:', userCount);
+userCount -= 3;
+console.log('Users count:', userCount);
+userCounttotal= userCount *= 2;
+console.log('Users count:', userCount);
+let mencount = userCount /= 2;
+let womencount = mencount
+console.log('Users counts:', userCounttotal, mencount, womencount);
+
+
