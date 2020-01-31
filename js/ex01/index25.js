94ad135c2c820c1a41819d00f4d319d022a01ce3
unknown <p_eghdami@yahoo.com>
2020-01-30 18:36:39 -0800

    index 25 added

---
+++ js/ex01/index25.js
@@ -0,0 +1,24 @@
+/* Create a new index25.js file
+We'll define a super hero character using variables and values
+Use the right data type for each variable
+Variable name: superHeroName, value: Batman
+Variable name: name, value: Bruce Wayne
+Variable name: fly, value: false
+Variable name: hasBatmobile, value: true
+Variable name: life, value: (value between 0 and 100)
+Variable name: freeTime, value: null
+Show each value using console.log() with the variable name (string) as first parameter and the actual variable value as second
+Example: console.log('name', name);
+Also show the variable names and the type of value it has assigned (use typeof)
+*/
+var superHeroName ='Batman';
+var fly = false;
+var hasBatmobile = true;
+var life = 60;
+var freeTime = null;
+console.log('superHeroName', superHeroName);
+console.log('fly', fly);
+console.log('hasBatmobile', hasBatmobile);
+console.log('life', life );
+console.log('freeTime', freetime);
+
