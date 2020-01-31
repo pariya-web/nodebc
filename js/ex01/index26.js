01280dc84c3c6ea46d5d897d2875c9f2ebc76394
unknown <p_eghdami@yahoo.com>
2020-01-30 18:37:04 -0800

    index 26 added

---
+++ js/ex01/index26.js
@@ -0,0 +1,20 @@
+/*Create a new index26.js file
+We need to define Winnipeg using variables and all the data types that we learned
+Declare at least 6 variables that will help you describe the city
+Try using at least one variable of each type
+Show each value using console.log() with the variable name (string) as first parameter and the actual variable value as second
+Example: console.log('name', name);
+Also show the variable names and the type of value it has assigned (use typeof)
+*/
+var cityName = 'Winnipeg';
+var location = 'capital of the Canadian province of Manitoba';
+var isCapitalOfManitoba = true;
+var population = 749534;
+var coldWinter = false;
+var populearForArcitecture = true;
+console.log('city Name:', cityName);
+console.log('location:', location);
+console.log('is Capital Of Manitoba', isCapitalOfManitoba);
+console.log('population:', population);
+console.log('cold Winter:', coldWinter);
+console.log('popular For Architechture', populearForArcitecture);
