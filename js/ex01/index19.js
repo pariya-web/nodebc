95321f3f081c8a0818d533a472172b73a51a2ae6
unknown <p_eghdami@yahoo.com>
2020-01-30 18:28:16 -0800

    index 19 added

---
+++ js/ex01/index19.js
@@ -0,0 +1,7 @@
+/*Create a new index19.js file
+Define a message variable and assign the following value: ComIT has the "best" programing courses
+Use the right quotes to define the variable
+Print the message value using console.log()
+*/
+var message = 'ComIT has the "the best" programming courses';
+console.log(message);
